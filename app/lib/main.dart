ccb8fdcc680e78e64d19649a7d7b6154c8aa24f0
kekland <kk.erzhan@gmail.com>
2026-08-15 14:46:35 +0500

    add wasm check

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,8 +1,16 @@
 import 'package:app/app/app.dart';
 import 'package:bindings/bindings.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/widgets.dart';
 
 void main() {
+  if (kIsWeb) {
+    final isRunningWithWasm = identical(double.nan, double.nan);
+
+    // ignore: avoid_print
+    print('wasm: $isRunningWithWasm');
+  }
+
   AugmentedWidgetsFlutterBinding.ensureInitialized();
   runApp(App());
 }
